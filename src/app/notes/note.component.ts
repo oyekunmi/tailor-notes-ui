07f4797fb28ec071563161de9ef1823a6fdf892a
Oyesola Ogundele <oyekunmi@gmail.com>
2018-12-19 14:14:16 +0100

    List theming

--- src/app/notes/note.component.ts
+++ src/app/notes/note.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Note } from './note.model';
 
-@Component({
+@Component({ 
   selector: 'app-note',
   template: `
     <article class="note-teaser">
       <div class="note-image">
-        <img src='' />
+        <mat-icon>person_pin</mat-icon>
       </div>
       <div class="note-content">
         <div class="note-ttle"> {{note.name}} </div>
         <div class="note-summary"> some text here </div>
-      </div>
-      <div class="note-full">
-        Full note information
+
+        <div class="note-full">
+          Full note information
+        </div>
+
       </div>
     </article>
   `,
-  styles: ['./note.component.scss']
+  styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
 
