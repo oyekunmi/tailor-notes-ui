491f4353ecde30b76ba4994717884101719e622e
Oyesola Ogundele <oyekunmi@gmail.com>
2018-12-24 08:33:12 +0100

    cleaned up for merging to master branch

--- src/app/notes/add-note.component.spec.ts
+++ src/app/notes/add-note.component.spec.ts
@@ -1,25 +1,82 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ContextService } from '../shared';
+import { FormBuilder } from '@angular/forms';
+import { NoteService } from './note.service';
+import { Router } from '@angular/router';
 import { AddNoteComponent } from './add-note.component';
 
 describe('AddNoteComponent', () => {
-  let component: AddNoteComponent;
-  let fixture: ComponentFixture<AddNoteComponent>;
+    let comp: AddNoteComponent;
+    let fixture: ComponentFixture<AddNoteComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddNoteComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        const contextServiceStub = {
+            showBackBtn: {
+                next: () => ({})
+            },
+            moduleTitle: {
+                next: () => ({})
+            }
+        };
+        const formBuilderStub = {
+            group: () => ({}),
+            array: () => ({})
+        };
+        const noteServiceStub = {
+            addNote: () => ({
+                subscribe: () => ({})
+            })
+        };
+        const routerStub = {
+            navigateByUrl: () => ({})
+        };
+        TestBed.configureTestingModule({
+            declarations: [ AddNoteComponent ],
+            schemas: [ NO_ERRORS_SCHEMA ],
+            providers: [
+                { provide: ContextService, useValue: contextServiceStub },
+                { provide: FormBuilder, useValue: formBuilderStub },
+                { provide: NoteService, useValue: noteServiceStub },
+                { provide: Router, useValue: routerStub }
+            ]
+        });
+        fixture = TestBed.createComponent(AddNoteComponent);
+        comp = fixture.componentInstance;
+    });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(AddNoteComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+    it('can load instance', () => {
+        expect(comp).toBeTruthy();
+    });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    describe('ngOnInit', () => {
+        it('makes expected calls', () => {
+            const formBuilderStub: FormBuilder = fixture.debugElement.injector.get(FormBuilder);
+            spyOn(comp, 'measureFactory');
+            spyOn(formBuilderStub, 'group');
+            spyOn(formBuilderStub, 'array');
+            comp.ngOnInit();
+            expect(comp.measureFactory).toHaveBeenCalled();
+            expect(formBuilderStub.group).toHaveBeenCalled();
+            expect(formBuilderStub.array).toHaveBeenCalled();
+        });
+    });
+
+    describe('measureFactory', () => {
+        it('makes expected calls', () => {
+            const formBuilderStub: FormBuilder = fixture.debugElement.injector.get(FormBuilder);
+            spyOn(formBuilderStub, 'group');
+            comp.measureFactory();
+            expect(formBuilderStub.group).toHaveBeenCalled();
+        });
+    });
+
+    describe('addMeasure', () => {
+        it('makes expected calls', () => {
+            spyOn(comp, 'measureFactory');
+            comp.addMeasure();
+            expect(comp.measureFactory).toHaveBeenCalled();
+        });
+    });
+
 });
